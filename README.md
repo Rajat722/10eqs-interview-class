a6437ddd5a756ccd4e159d2d9e3ab6487580c2cf
Rajat Gaur <rajatgaur722@gmai.com>
2024-12-23 15:44:20 -0500

    added some analysis rest i was out of time

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # 10eqs-interview
+### Project: Product Data Analysis
+
+# README
+
+## Overview
+
+This project helps a small business owner track their product data by reading a provided CSV file and transforming it into a pandas DataFrame. The tool enables further analysis and visualization of the product data to generate actionable insights.
+
+## Setup Instructions
+
+1. **Clone the Repository**:
+   ```sh
+   git clone https://github.com/your-username/product-data-analysis.git
+   cd product-data-analysis
+   ```
+2. **Create Virtual Environment**:
+
+    venv\Scripts\activate
+
+3. **Activate Virtual Environment**:
+    source venv/bin/activate
+
+4. **Install Dependencies**:
+    pip install -r requirements.txt
+
+
+5. **Run the Project**:
+```sh
+    python src/analysis.py data/products.csv    
+```
--- src/analysis.py
+++ src/analysis.py
@@ -3,7 +3,7 @@ import sys
 import requests
 from config import API_KEY
 from utils import read_csv_to_dataframe
-
+import seaborn as sns
 
 
 
@@ -16,8 +16,30 @@ def main():
         sys.exit(1)
 
     file_path = sys.argv[1]
-    products_df = read_csv_to_dataframe(file_path)
-    
+    df = read_csv_to_dataframe(file_path)
+    # Analysis
+    # lets see how the dataset looks like-
+    print(df)
+    # I can notice there was some error while extracting the data set from the pdf document
+    # provided by 10eqs as the last column of supplier is not extarcted from the pdf
+    col_names = df.columns.to_list()
+    print(col_names)
+    # check for null values
+    print(df.isnull().sum())
+    # As we can see there are 2 NaN/null values present in our_price and restock_threshold feature so we are just going
+    # to drop those row
+    df.dropna(inplace=True)
+    print(df.isnull().sum())
+
+    # I will use seaborn to check for correlations between features so we can do some feature 
+    # engineering
+    print(df.corr())
+    sns.pairplot(df)
+
+    # From the pairplot we can observe that there is a strong correlation between current stock and 
+    # restock_threshold for Chamomile Tea (30 bags)
+
+
 
 
 def fetch_data():
@@ -32,5 +54,5 @@ def fetch_data():
 
 if __name__ == "__main__":
     main()
-    data = fetch_data()
-    print(data)
+    # data = fetch_data()
+    # print(data)
